c18189996e8dca8cae87d5ff7271a36b46ddc468
eubrunorossato <Bruno.Araujo1@ibm.com>
2021-08-26 00:17:14 -0300

    set up box style

--- src/components/pages/books/index.js
+++ src/components/pages/books/index.js
@@ -1,35 +1,38 @@
 import React from 'react';
-import { Row, Col, Container } from 'react-materialize';
 import Button from '../../shared/button/index';
 import Card from '../../shared/cards/index';
+import axios from 'axios';
+import { Row, Col, Container } from 'react-materialize';
 import './_books.css'
 
 export default function () {
   return (
     <Container>
-      <Row>
-        <Col
-          m={6}
-          s={12}
-        >
-          <Card />
+      <div className="box">
+        <Row>
+          <Col
+            m={6}
+            s={12}
+          >
+            <Card />
 
-        </Col>
-        <Col
-          m={6}
-          s={12}
-        >
-          <div className="actionPanel">
-            <p>Avaliables:</p>
-            <div className="buttonDivs">
-              <Button iconName="book_online" buttonLabel="Book" />
+          </Col>
+          <Col
+            m={6}
+            s={12}
+          >
+            <div className="actionPanel">
+              <p>Avaliables:</p>
+              <div className="buttonDivs">
+                <Button iconName="book_online" buttonLabel="Book" />
+              </div>
+              <div className="buttonDivs">
+                <Button iconName="volunteer_activism" buttonLabel="Donate same book" />
+              </div>
             </div>
-            <div className="buttonDivs">
-              <Button iconName="volunteer_activism" buttonLabel="Donate same book" />
-            </div>
-          </div>
-        </Col>
-      </Row >
+          </Col>
+        </Row >
+      </div>
     </Container >
   )
 }
