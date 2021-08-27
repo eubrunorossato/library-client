d4dc6c474cbcaf80409d3cdcf981426c0670bd4e
eubrunorossato <Bruno.Araujo1@ibm.com>
2021-08-27 01:41:52 -0300

    add ; at hte end of the line

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css';
-import 'material-design-icons'
-import Routes from './Router'
-import Header from './components/shared/header/header'
+import 'material-design-icons';
+import Routes from './Router';
+import Header from './components/shared/header/header';
 function App() {
   return (
     <div className="App">
