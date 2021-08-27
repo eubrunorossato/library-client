2502661dc56114ab495e4c2f0ad7f3f0ed49e72d
eubrunorossato <Bruno.Araujo1@ibm.com>
2021-08-27 19:44:31 -0300

    set up props

--- src/components/shared/cards/index.js
+++ src/components/shared/cards/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Card, CardTitle, Icon } from 'react-materialize';
 
-const CardsShared = () => {
+const CardsShared = (props) => {
   return (
     <Card
       actions={[
-        <a key="1" href="#">This is a link</a>
+        <p>{props.bookName}</p>
       ]}
       closeIcon={<Icon>close</Icon>}
       header={<CardTitle image="https://materializecss.com/images/sample-1.jpg" />}
       horizontal
       revealIcon={<Icon>more_vert</Icon>}
     >
-      Here is the standard card with a horizontal image.
+      {props.bookResume}
     </Card>
   )
 };
