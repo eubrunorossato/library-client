8102b0f01f5844f2073b0bd6c9e23ff097677b79
eubrunorossato <Bruno.Araujo1@ibm.com>
2021-08-27 20:50:44 -0300

    render complete book info

--- src/components/pages/books/index.js
+++ src/components/pages/books/index.js
@@ -1,51 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import Button from '../../shared/button/index';
 import Card from '../../shared/cards/index';
-import axios from '../../../helpers/axios';
+import { axiosInstance, convertBufferToImage } from '../../../helpers';
 import { Row, Col, Container } from 'react-materialize';
 import './_books.css';
 
+const renderBookList = (bookList) => {
+  const imageBase64List = getImagesBase64(bookList);
+  return bookList.map((book, index) => (
+    <div key={book.id} className="box">
+      <Row>
+        <Col
+          m={6}
+          s={12}
+        >
+          <Card keyId={book.id} imageBase64={imageBase64List[index]} bookName={book.name} bookResume={book.resume} />
+        </Col>
+        <Col
+          m={6}
+          s={12}
+        >
+          <div className="actionPanel">
+            <p>Avaliables to book: <span>{book.avaliables}</span></p>
+            <div className="buttonDivs">
+              <Button iconName="book_online" buttonLabel="Book" />
+            </div>
+            <div className="buttonDivs">
+              <Button iconName="volunteer_activism" buttonLabel="Donate same book" />
+            </div>
+          </div>
+        </Col>
+      </Row >
+    </div>
+  ))
+};
+
+const getImagesBase64 = (bookList) => {
+  const base64String = [];
+  bookList.map(book => {
+    base64String.push(convertBufferToImage(book.book_picture.data));
+  });
+  return base64String;
+};
 
 const Books = () => {
   const [bookList, setBookList] = useState([]);
-  function renderBookList() {
-    return bookList.map(book => (
-      <div key={book.id} className="box">
-        <Row>
-          <Col
-            m={6}
-            s={12}
-          >
-            <Card bookName={book.name} bookResume={book.resume} />
-          </Col>
-          <Col
-            m={6}
-            s={12}
-          >
-            <div className="actionPanel">
-              <p>Avaliables to book: <span>{book.avaliables}</span></p>
-              <div className="buttonDivs">
-                <Button iconName="book_online" buttonLabel="Book" />
-              </div>
-              <div className="buttonDivs">
-                <Button iconName="volunteer_activism" buttonLabel="Donate same book" />
-              </div>
-            </div>
-          </Col>
-        </Row >
-      </div>
-    ))
-  }
   useEffect(() => {
     async function fetchBookList() {
-      const { data } = await axios.get('/api/books/getAll');
+      const { data } = await axiosInstance.get('/api/books/getAll');
       setBookList(data.books);
     }
     fetchBookList();
   }, []);
   return (
     <Container>
-      {renderBookList()}
+      {renderBookList(bookList)}
     </Container >
   )
 }
