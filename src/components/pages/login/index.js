1cf63c4f582a824e37b57a3ccdd69e76d06b4062
eubrunorossato <Bruno.Araujo1@ibm.com>
2021-09-30 00:51:18 -0300

    remove comments

--- src/components/pages/login/index.js
+++ src/components/pages/login/index.js
@@ -3,10 +3,10 @@ const LoginPage = () => {
     function onSignIn (googleUser) {
         const profile = googleUser.getBasicProfile();
         console.log(profile);
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('ID: ' + profile.getId()); 
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        console.log('Email: ' + profile.getEmail());
       }
       
     return (
